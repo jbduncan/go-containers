19bb3e9e1b1fa9c7a680d6344063e9a06d65e04b
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2022-03-25 18:19:54 +0000

    Clean up initial graph API

--- container/graph.go
+++ container/graph.go
@@ -13,12 +13,12 @@ type Graph[N comparable] interface {
 	AdjacentNodes() Set[N]
 	Predecessors() Set[N]
 	Successors() Set[N]
-	IncidentEdges(n N) Set[EndpointPair[N]]
-	Degree(n N) uint
-	InDegree(n N) uint
-	OutDegree(n N) uint
-	HasEdgeConnecting(u N, v N) bool
-	HasEdgeConnectingEndpoints(e EndpointPair[N]) bool
+	IncidentEdges(node N) Set[EndpointPair[N]]
+	Degree(node N) int
+	InDegree(node N) int
+	OutDegree(node N) int
+	HasEdgeConnecting(nodeU N, nodeV N) bool
+	HasEdgeConnectingEndpoints(endpointPair EndpointPair[N]) bool
 	String() string
 	// TODO: Is an Equals function needed to meet Guava's Graph::equals rules?
 }
@@ -37,86 +37,72 @@ type MutableGraph[N comparable] interface {
 // TODO: Implement
 // type ElementOrder struct {}
 
-func NewUnorderedEndpointPair[N comparable](s N, t N) EndpointPair[N] {
+func NewUnorderedEndpointPair[N comparable](nodeU N, nodeV N) EndpointPair[N] {
 	return EndpointPair[N]{
-		u: s,
-		v: t,
+		nodeU:     nodeU,
+		nodeV:     nodeV,
+		isOrdered: false,
+	}
+}
+
+func NewOrderedEndpointPair[N comparable](source N, target N) EndpointPair[N] {
+	return EndpointPair[N]{
+		nodeU:     source,
+		nodeV:     target,
+		isOrdered: true,
 	}
 }
 
 type EndpointPair[N comparable] struct {
-	u N
-	v N
+	nodeU     N
+	nodeV     N
+	isOrdered bool
+}
+
+func (e EndpointPair[N]) Source() N {
+	if e.isOrdered {
+		return e.nodeU
+	}
+	panic("cannot call Source()/Target() on an EndpointPair from an undirected graph; consider calling " +
+		"AdjacentNode(node) if you already have a node, or NodeU()/NodeV() if you don't")
+}
+
+func (e EndpointPair[N]) Target() N {
+	if e.isOrdered {
+		return e.nodeV
+	}
+	panic("cannot call Source()/Target() on an EndpointPair from an undirected graph; consider calling " +
+		"AdjacentNode(node) if you already have a node, or NodeU()/NodeV() if you don't")
 }
 
 func (e EndpointPair[N]) NodeU() N {
-	return e.u
+	return e.nodeU
 }
 
 func (e EndpointPair[N]) NodeV() N {
-	return e.v
+	return e.nodeV
 }
 
-func (e EndpointPair[N]) AdjacentNode(n N) (N, error) {
-	if n == e.u {
-		return e.v, nil
+func (e EndpointPair[N]) AdjacentNode(n N) N {
+	if n == e.nodeU {
+		return e.nodeV
 	}
-	if n == e.v {
-		return e.u, nil
+	if n == e.nodeV {
+		return e.nodeU
 	}
-	return zeroValue[N](), fmt.Errorf("EndpointPair %v does not contain node %#v", e.String(), n)
+	panic(fmt.Sprintf("EndpointPair %v does not contain node %v", e, n))
 }
 
 func (e EndpointPair[N]) IsOrdered() bool {
-	return false
+	return e.isOrdered
 }
+
+// TODO: EndpointPair: make Equals method and discourage == from being used. See this link:
+//       https://github.com/google/guava/blob/4d323b2b117a5906ab16074c8c88b4ff162b1b82/guava/src/com/google/common/graph/EndpointPair.java#L131-L145
 
 func (e EndpointPair[N]) String() string {
-	return fmt.Sprintf("[%#v, %#v]", e.NodeU(), e.NodeV())
-}
-
-func NewOrderedEndpointPair[N comparable](s N, t N) OrderedEndpointPair[N] {
-	return OrderedEndpointPair[N]{
-		u: s,
-		v: t,
+	if e.isOrdered {
+		return fmt.Sprintf("<%v -> %v>", e.Source(), e.Target())
 	}
-}
-
-type OrderedEndpointPair[N comparable] struct {
-	u N
-	v N
-}
-
-func (o OrderedEndpointPair[N]) Source() N {
-	return o.NodeU()
-}
-
-func (o OrderedEndpointPair[N]) Target() N {
-	return o.NodeV()
-}
-
-func (o OrderedEndpointPair[N]) NodeU() N {
-	return o.u
-}
-
-func (o OrderedEndpointPair[N]) NodeV() N {
-	return o.v
-}
-
-func (o OrderedEndpointPair[N]) AdjacentNode(n N) (N, error) {
-	if n == o.u {
-		return o.v, nil
-	}
-	if n == o.v {
-		return o.u, nil
-	}
-	return zeroValue[N](), fmt.Errorf("OrderedEndpointPair %v does not contain node %#v", o.String(), n)
-}
-
-func (o OrderedEndpointPair[N]) IsOrdered() bool {
-	return true
-}
-
-func (o OrderedEndpointPair[N]) String() string {
-	return fmt.Sprintf("<%#v -> %#v>", o.Source(), o.Target())
+	return fmt.Sprintf("[%v, %v]", e.NodeU(), e.NodeV())
 }
