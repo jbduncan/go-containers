fb04b8b3b71109f7823353cc71142cbbe4ef0a99
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2023-05-06 18:47:14 +0100

    Improve error messaging of BeEquivalentToUsingEqualMethod

--- internal/matchers/be_equivalent_to_using_equal_method.go
+++ internal/matchers/be_equivalent_to_using_equal_method.go
@@ -4,45 +4,45 @@ import (
 	"fmt"
 	"reflect"
 
-	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/format"
+	"github.com/onsi/gomega/gcustom"
 	"github.com/onsi/gomega/types"
 )
 
-func BeEquivalentToUsingEqualMethod(value any) types.GomegaMatcher {
-	return WithTransform(
-		func(this any) (bool, error) {
-			typ := reflect.TypeOf(this)
-			noEqualMethodMessage := "to have an Equal method with a single parameter of type <" +
-				typ.Name() +
-				"> and a single return value of type <bool>"
-			errNoEqualMethod := fmt.Errorf(format.Message(this, noEqualMethodMessage))
+const noEqualMethodMessageFormat = "<%T> is expected to have an Equal " +
+	"method with a single parameter of type <%T> and a single return value " +
+	"of type <bool>"
 
-			equalMethod, ok := typ.MethodByName("Equal")
-			if !ok {
-				return false, errNoEqualMethod
-			}
+func BeEquivalentToUsingEqualMethod(expected any) types.GomegaMatcher {
+	return gcustom.MakeMatcher(func(actual any) (bool, error) {
+		typ := reflect.TypeOf(actual)
 
-			if !hasReceiverAndParamOfSameType(equalMethod.Type) {
-				return false, errNoEqualMethod
-			}
+		equalMethod, ok := typ.MethodByName("Equal")
+		if !ok {
+			return false, fmt.Errorf(noEqualMethodMessageFormat, expected, expected)
+		}
 
-			if equalMethod.Type.NumOut() != 1 {
-				return false, errNoEqualMethod
-			}
+		if !hasReceiverAndParamOfSameType(equalMethod.Type) {
+			return false, fmt.Errorf(noEqualMethodMessageFormat, expected, expected)
+		}
 
-			if !equalMethod.Type.Out(0).AssignableTo(reflect.TypeOf(true)) {
-				return false, errNoEqualMethod
-			}
+		if equalMethod.Type.NumOut() != 1 {
+			return false, fmt.Errorf(noEqualMethodMessageFormat, expected, expected)
+		}
 
-			params := []reflect.Value{
-				reflect.ValueOf(this),
-				reflect.ValueOf(value),
-			}
-			result := equalMethod.Func.Call(params)[0].Bool()
-			return result, nil
-		},
-		BeTrue())
+		if !equalMethod.Type.Out(0).AssignableTo(reflect.TypeOf(true)) {
+			return false, fmt.Errorf(noEqualMethodMessageFormat, expected, expected)
+		}
+
+		params := []reflect.Value{
+			reflect.ValueOf(actual),
+			reflect.ValueOf(expected),
+		}
+		result := equalMethod.Func.Call(params)[0].Bool()
+		return result, nil
+	}).WithTemplate(
+		"Expected:\n{{.FormattedActual}}\n{{.To}} be equivalent to "+
+			"according to .Equal():\n{{format .Data 1}}",
+		expected)
 }
 
 func hasReceiverAndParamOfSameType(methodType reflect.Type) bool {
