06d4c7c7525131b36d1f63cd566b1832a301238b
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2023-12-08 16:39:04 +0000

    Fix TestStringImpl

--- set/string_impl_test.go
+++ set/string_impl_test.go
@@ -4,44 +4,36 @@ import (
 	"testing"
 
 	"github.com/jbduncan/go-containers/set"
+	"golang.org/x/exp/slices"
 )
 
 func TestStringImpl(t *testing.T) {
-	type args struct {
-		s set.Set[string]
-	}
 	type testCase struct {
-		name string
-		args args
-		want string
+		name    string
+		arg     set.Set[string]
+		wantAny []string
 	}
 	tests := []testCase{
 		{
-			name: "empty set",
-			args: args{
-				s: set.New[string](),
-			},
-			want: "[]",
+			name:    "empty set",
+			arg:     set.New[string](),
+			wantAny: []string{"[]"},
 		},
 		{
-			name: "one element set",
-			args: args{
-				s: oneElementSet(),
-			},
-			want: "[link]",
+			name:    "one element set",
+			arg:     oneElementSet(),
+			wantAny: []string{"[link]"},
 		},
 		{
-			name: "two element set",
-			args: args{
-				s: twoElementSet(),
-			},
-			want: "[link, zelda]",
+			name:    "two element set",
+			arg:     twoElementSet(),
+			wantAny: []string{"[link, zelda]", "[zelda, link]"},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := set.StringImpl(tt.args.s); got != tt.want {
-				t.Errorf("StringImpl() = %v, want %v", got, tt.want)
+			if got := set.StringImpl(tt.arg); !slices.Contains(tt.wantAny, got) {
+				t.Errorf("StringImpl() = %v, wantAny any of %v", got, tt.wantAny)
 			}
 		})
 	}
