9b78fab1fcd181a7ec426c97e8e6904dfd549a21
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2024-01-26 17:45:20 +0000

    Improve documentation

--- set/doc.go
+++ set/doc.go
@@ -1,11 +1,13 @@
 // Package set provides a set data structure, which is a generic, unordered container of elements where no two elements
 // can be equal according to Go's == operator.
 //
-// An immutable set can be created with Of. A mutable set can be created with NewMutable.
+// An immutable set can be created with Of. Likewise, a mutable set can be created with NewMutable.
 //
-// An existing mutable set can be passed into Unmodifiable, which turns it into a read-only Set view.
+// A mutable set can be passed into Unmodifiable, which turns it into a read-only Set view.
 //
-// Sets have a String method that satisfies fmt.Stringer.
+// Set has a String method that satisfies fmt.Stringer.
+//
+// The union of two sets can be created with Union.
 //
 // Two sets can be compared for "equality" with Equal, returning true if they have the same elements in any order,
 // otherwise false.
