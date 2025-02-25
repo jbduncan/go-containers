b9226bc9f647e0510484ed8d5d476d89fdf15b72
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2025-02-25 17:46:13 +0000

    Refactoring

--- graph/graphtest/tester.go
+++ graph/graphtest/tester.go
@@ -4,9 +4,7 @@ import (
 	"slices"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
 	"github.com/jbduncan/go-containers/graph"
-	"github.com/jbduncan/go-containers/internal/orderagnostic"
 	"github.com/jbduncan/go-containers/set"
 )
 
@@ -475,47 +473,4 @@ func testDegree(
 			want,
 		)
 	}
-}
-
-func reversesOf(edges []graph.EndpointPair[int]) []graph.EndpointPair[int] {
-	result := make([]graph.EndpointPair[int], 0, len(edges))
-	for _, edge := range edges {
-		result = append(result, reverseOf(edge))
-	}
-	return result
-}
-
-func undirectedEndpointPairsDiff(
-	got []graph.EndpointPair[int],
-	want []graph.EndpointPair[int],
-) string {
-	return cmp.Diff(
-		want,
-		got,
-		cmp.Comparer(
-			func(
-				a []graph.EndpointPair[int],
-				b []graph.EndpointPair[int],
-			) bool {
-				aCopy, bCopy := deepCopyAndNormalise(a), deepCopyAndNormalise(b)
-				return orderagnostic.SlicesEqual(aCopy, bCopy)
-			},
-		),
-	)
-}
-
-func deepCopyAndNormalise(
-	s []graph.EndpointPair[int],
-) []graph.EndpointPair[int] {
-	var result []graph.EndpointPair[int]
-	for _, edge := range s {
-		var newEdge graph.EndpointPair[int]
-		if edge.Source() < edge.Target() {
-			newEdge = graph.EndpointPairOf(edge.Source(), edge.Target())
-		} else {
-			newEdge = graph.EndpointPairOf(edge.Target(), edge.Source())
-		}
-		result = append(result, newEdge)
-	}
-	return result
 }
--- graph/graphtest/util.go
+++ graph/graphtest/util.go
@@ -3,11 +3,21 @@ package graphtest
 import (
 	"strings"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/jbduncan/go-containers/graph"
+	"github.com/jbduncan/go-containers/internal/orderagnostic"
 )
 
 func reverseOf(endpointPair graph.EndpointPair[int]) graph.EndpointPair[int] {
 	return graph.EndpointPairOf(endpointPair.Target(), endpointPair.Source())
+}
+
+func reversesOf(edges []graph.EndpointPair[int]) []graph.EndpointPair[int] {
+	result := make([]graph.EndpointPair[int], 0, len(edges))
+	for _, edge := range edges {
+		result = append(result, reverseOf(edge))
+	}
+	return result
 }
 
 func splitByComma(s string) []string {
@@ -15,4 +25,39 @@ func splitByComma(s string) []string {
 		return make([]string, 0)
 	}
 	return strings.Split(s, ", ")
+}
+
+func undirectedEndpointPairsDiff(
+	got []graph.EndpointPair[int],
+	want []graph.EndpointPair[int],
+) string {
+	return cmp.Diff(
+		want,
+		got,
+		cmp.Comparer(
+			func(
+				a []graph.EndpointPair[int],
+				b []graph.EndpointPair[int],
+			) bool {
+				aCopy, bCopy := deepCopyAndNormalise(a), deepCopyAndNormalise(b)
+				return orderagnostic.SlicesEqual(aCopy, bCopy)
+			},
+		),
+	)
+}
+
+func deepCopyAndNormalise(
+	s []graph.EndpointPair[int],
+) []graph.EndpointPair[int] {
+	var result []graph.EndpointPair[int]
+	for _, edge := range s {
+		var newEdge graph.EndpointPair[int]
+		if edge.Source() < edge.Target() {
+			newEdge = graph.EndpointPairOf(edge.Source(), edge.Target())
+		} else {
+			newEdge = graph.EndpointPairOf(edge.Target(), edge.Source())
+		}
+		result = append(result, newEdge)
+	}
+	return result
 }
