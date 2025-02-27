6e9308550465198419c76d06f3c4a92a4f75e7ed
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2025-02-27 01:28:13 +0000

    Formatting

--- internal/orderagnostic/diff.go
+++ internal/orderagnostic/diff.go
@@ -1,16 +1,7 @@
 package orderagnostic
 
-import (
-	"github.com/google/go-cmp/cmp"
-)
+import "github.com/google/go-cmp/cmp"
 
-func Diff[T comparable](
-	got []T,
-	want []T,
-) string {
-	return cmp.Diff(
-		want,
-		got,
-		cmp.Comparer(SlicesEqual[T]),
-	)
+func Diff[T comparable](got []T, want []T) string {
+	return cmp.Diff(want, got, cmp.Comparer(SlicesEqual[T]))
 }
