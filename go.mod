b6f167de77f57995884f7aeebd5b2b3c87e7f5a1
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2025-04-04 18:37:30 +0100

    Remove last vestiges of Gomega

--- go.mod
+++ go.mod
@@ -6,17 +6,5 @@ toolchain go1.24.1
 
 require (
 	github.com/google/go-cmp v0.7.0
-	github.com/onsi/gomega v1.36.3
 	golang.org/x/sync v0.12.0
-)
-
-require (
-	github.com/google/pprof v0.0.0-20250302191652-9094ed2288e7 // indirect
-	github.com/kr/pretty v0.3.1 // indirect
-	github.com/rogpeppe/go-internal v1.13.1 // indirect
-	golang.org/x/net v0.38.0 // indirect
-	golang.org/x/text v0.23.0 // indirect
-	golang.org/x/tools v0.31.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- set/equal_test.go
+++ set/equal_test.go
@@ -4,33 +4,36 @@ import (
 	"testing"
 
 	"github.com/jbduncan/go-containers/set"
-	. "github.com/onsi/gomega"
 )
 
 func TestEqual(t *testing.T) {
 	t.Run("set a: nil; set b: nil; equal", func(t *testing.T) {
-		g := NewWithT(t)
-
-		g.Expect(set.Equal[string](nil, nil)).To(BeTrue())
+		if got := set.Equal[string](nil, nil); !got {
+			t.Errorf("set.Equal: got false, want true")
+		}
 	})
 
 	t.Run("set a: [link]; set b: nil; not equal", func(t *testing.T) {
-		g := NewWithT(t)
-
-		g.Expect(set.Equal(set.Of("link"), nil)).To(BeFalse())
+		if got := set.Equal[string](set.Of("link"), nil); got {
+			t.Errorf("set.Equal: got true, want false")
+		}
 	})
 
 	t.Run("set a: [link]; set b: [zelda]; not equal", func(t *testing.T) {
-		g := NewWithT(t)
-
-		g.Expect(set.Equal(set.Of("link"), set.Of("zelda"))).To(BeFalse())
+		if got := set.Equal[string](set.Of("link"), set.Of("zelda")); got {
+			t.Errorf("set.Equal: got true, want false")
+		}
 	})
 
-	t.Run("set a: [link]; set b: [link, zelda]; not equal", func(t *testing.T) {
-		g := NewWithT(t)
-
-		g.Expect(set.Equal(set.Of("link", "zelda"), set.Of("link"))).To(BeFalse())
-	})
+	t.Run(
+		"set a: [link]; set b: [link, zelda]; not equal",
+		func(t *testing.T) {
+			got := set.Equal[string](set.Of("link"), set.Of("link", "zelda"))
+			if got {
+				t.Errorf("set.Equal: got true, want false")
+			}
+		},
+	)
 }
 
 func FuzzEquals(f *testing.F) {
@@ -43,12 +46,24 @@ func FuzzEquals(f *testing.F) {
 	f.Add([]byte("0"), []byte("00"))
 
 	f.Fuzz(func(t *testing.T, bytesA []byte, bytesB []byte) {
-		g := NewWithT(t)
 		a := set.Of(bytesA...)
 		b := set.Of(bytesB...)
 
-		g.Expect(set.Equal(a, a)).To(BeTrue())
-		g.Expect(set.Equal(a, b)).To(Equal(set.Equal(b, a)))
-		g.Expect(set.Equal(a, b)).To(Equal(set.Equal(a, b)))
+		if got := set.Equal[byte](a, a); !got {
+			t.Errorf("set.Equal(a, a): got false, want true")
+		}
+		if got := set.Equal[byte](a, b) == set.Equal[byte](b, a); !got {
+			t.Errorf(
+				"set.Equal(a, b) == set.Equal(b, a): got false, want true",
+			)
+		}
+		// Purposefully checking that two logically identical expressions
+		// should always return the same result.
+		//nolint:revive,staticcheck
+		if got := set.Equal[byte](a, b) == set.Equal[byte](a, b); !got {
+			t.Errorf(
+				"set.Equal(a, b) == set.Equal(a, b): got false, want true",
+			)
+		}
 	})
 }
