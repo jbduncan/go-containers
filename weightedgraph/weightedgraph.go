6493c0ac4150a49bc59d0972abf3098a7303dc2f
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2025-05-09 16:59:35 +0100

    Introduce start of WeightGraph skeleton

---
+++ weightedgraph/weightedgraph.go
@@ -0,0 +1,20 @@
+package weightedgraph
+
+func Undirected[N comparable]() Builder[N] {
+	return Builder[N]{
+		directed:        false,
+		allowsSelfLoops: false,
+	}
+}
+
+type Builder[N comparable] struct {
+	directed        bool
+	allowsSelfLoops bool
+}
+
+func (b Builder[N]) Build() *WeightedGraph[N] {
+	return &WeightedGraph[N]{}
+}
+
+type WeightedGraph[N comparable] struct {
+}
